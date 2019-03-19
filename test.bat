6164c871c49555b62277c1d01ac393180aa13032
Satish <s.biharie@ultimaker.com>
2019-03-19 15:10:23 +0100

    Processed test results

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 START C:\Users\s.biharie\"Squish for Qt 6.4-20190117-1156"\bin\squishserver.exe
 CD C:\Users\s.biharie\"Squish for Qt 6.4-20190117-1156"\bin\
-squishrunner --testsuite C:\Users\s.biharie\Develop\Squish\suite_Cura
+squishrunner --testsuite C:\Users\s.biharie\Develop\Squish\suite_Cura --reportgen html,/tmp/results
 CD C:\Users\s.biharie\Develop\Squish\
