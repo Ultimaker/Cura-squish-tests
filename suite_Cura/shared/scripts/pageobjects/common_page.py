fc83f9526e28daa35bc11e19b5780a04255695cc
Satish <s.biharie@ultimaker.com>
2019-03-25 14:59:03 +0100

    Added loop to make sure cura config is removed before copying testdata there Removed unused features Cleaned up gitlab config

--- suite_Cura/shared/scripts/pageobjects/common_page.py
+++ suite_Cura/shared/scripts/pageobjects/common_page.py
@@ -43,13 +43,10 @@ class PageObject:
             shutil.rmtree(self.linuxDir["config"], ignore_errors=True)
             
     def presetPreferences(self):
-        self.resetPreferences()    
-        if self.os == "Windows":
-            shutil.copytree(findFile("testdata", "WindowsConfig/4.0"), self.windowsDir)    
-        elif self.os == "Linux":
-            shutil.copytree(findFile("testdata", "WindowsConfig/4.0"), self.linuxDir["local"]) 
-            shutil.copytree(findFile("testdata", "WindowsConfig/4.0"), self.linuxDir["config"]) 
-
+#         Make sure preferences are completely deleted before copying to that dir
+        while os.path.isdir(self.windowsDir):
+            self.resetPreferences()
+        
         # Set the CWD to the testdata folder
         configFile = findFile("testdata", "WindowsConfig/4.0/cura.cfg")
         testdataDir = os.path.join(os.getcwd(), findFile("testdata", ""))
@@ -65,7 +62,15 @@ class PageObject:
                 if line == "[local_file]\n":
                     line = line + "dialog_load_path = " + testdataDir + "\ndialog_save_path = " + testdataDir + "\n"
                     
-                new_file.write(line)
+                new_file.write(line) 
+         
+        if self.os == "Windows":
+            shutil.copytree(findFile("testdata", "WindowsConfig/4.0"), self.windowsDir)    
+        elif self.os == "Linux":
+            shutil.copytree(findFile("testdata", "WindowsConfig/4.0"), self.linuxDir["local"]) 
+            shutil.copytree(findFile("testdata", "WindowsConfig/4.0"), self.linuxDir["config"]) 
+
+        
                     
     def setTextFieldValue(self, object, value):
         if self.os in ("Windows", "Linux"):
--- suite_Cura/tst_Auto_Slicing/test.feature
+++
@@ -1,8 +0,0 @@
-Feature: Auto slicing
-
-    Scenario: Enable auto slicing
-        Given Cura has been started with a pre-loaded model
-        When I navigate to general preferences
-        And I enable "Slice automatically"
-        And I exit general preferences
-        Then the model has been sliced automatically
