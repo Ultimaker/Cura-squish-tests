871c17cb36ade1839d1dce63e1850ba8117efd19
Ellecross <32929710+Ellecross@users.noreply.github.com>
2019-03-25 16:33:20 +0100

    Add market place files that accidently got left out.

---
+++ suite_Cura/shared/steps/marketplace_steps.py
@@ -0,0 +1,14 @@
+from pageobjects.marketplace_page import Marketplace
+
+marketplace = Marketplace()
+
+@Then("I can verify that the plugin is installed")
+def step(context):
+    marketplace.verifyPluginInstalled()
+    
+
+@When(r"I select plugin 'Custom Supports'?(.*)", regexp=True)
+def step(context, action):
+    marketplace.selectPlugin('Custom Supports')
+    if "and install it" in action:     
+        marketplace.selectPluginInstall()
