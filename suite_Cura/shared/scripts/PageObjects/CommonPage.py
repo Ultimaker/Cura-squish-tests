74b5d5745b03f5581cdb6b050c456e6a7ea872c4
evtrados <evtrados@gmail.com>
2020-07-10 11:40:57 +0200

    Commenting the lines that would close the changelog, since they were interfering with other test cases (e.g., Onboarding flow).

--- suite_Cura/shared/scripts/PageObjects/CommonPage.py
+++ suite_Cura/shared/scripts/PageObjects/CommonPage.py
@@ -50,8 +50,8 @@ class PageObject:
         startApplication(aut)
         
         waitForObject(names.mwi, 50000)
-        if object.exists(names.mwi_changelog):
-            self.click(names.mwi_changelog_btn_close)
+        #if object.exists(names.mwi_changelog):
+        #    self.click(names.mwi_changelog_btn_close)
         
     def startCuraWithArguments(self):
         
